03469bde87f040cedab06ab84f3163d952b26411
Polina Kachan <kachan.polina8@gmail.com>
2026-05-13 20:23:51 +0300

    Change values names

--- src/index.js
+++ src/index.js
@@ -1,149 +1,146 @@
 'use strict';
 
-const divFormContainer = document.createElement('div');
-divFormContainer.className = 'form-container';
-document.body.append(divFormContainer);
+const formContainer = document.createElement('div');
+formContainer.className = 'form-container';
+document.body.append(formContainer);
 
-const divTitle = document.createElement('div');
-divTitle.className = 'title';
-divFormContainer.append(divTitle);
+const title = document.createElement('div');
+title.className = 'title';
+formContainer.append(title);
 
 const titleH2 = document.createElement('h2');
 titleH2.textContent = 'CREATE AN ACCOUNT';
-divTitle.append(titleH2);
+title.append(titleH2);
 
 const titleP = document.createElement('p');
 titleP.textContent = 'We always keep your name and email adress private';
-divTitle.append(titleP);
+title.append(titleP);
 
 const form = document.createElement('form');
-divFormContainer.append(form);
+formContainer.append(form);
 
+// Inputs
+const inputContainer = document.createElement('div');
+inputContainer.className = 'input-container';
+form.append(inputContainer);
 
-const formDivInputContainer = document.createElement('div');
-formDivInputContainer.className = 'input-container';
-form.append(formDivInputContainer);
-
-const formInputFieldFirst = document.createElement('div');
-formInputFieldFirst.className = 'input-field';
-formDivInputContainer.append(formInputFieldFirst);
+const firstInputFields = document.createElement('div');
+firstInputFields.className = 'input-field';
+inputContainer.append(firstInputFields);
 
 const firstInput = document.createElement('input');
-formInputFieldFirst.append(firstInput);
+firstInputFields.append(firstInput);
 firstInput.setAttribute('type', 'text');
 firstInput.setAttribute('name', 'first-name');
 firstInput.setAttribute('id', 'first-name');
 firstInput.setAttribute('placeholder', 'First name');
 
 const secondInput = document.createElement('input');
-formInputFieldFirst.append(secondInput);
+firstInputFields.append(secondInput);
 secondInput.setAttribute('type', 'text');
 secondInput.setAttribute('name', 'last-name');
 secondInput.setAttribute('id', 'last-name');
 secondInput.setAttribute('placeholder', 'Last name');
 
-
-const formInputFieldSecond = document.createElement('div');
-formInputFieldSecond.className = 'input-field';
-formDivInputContainer.append(formInputFieldSecond);
+const secondInputFields = document.createElement('div');
+secondInputFields.className = 'input-field';
+inputContainer.append(secondInputFields);
 
 const thirdInput = document.createElement('input');
-formInputFieldSecond.append(thirdInput);
+secondInputFields.append(thirdInput);
 thirdInput.setAttribute('type', 'text');
 thirdInput.setAttribute('name', 'first-name');
 thirdInput.setAttribute('id', 'first-name');
 thirdInput.setAttribute('placeholder', 'First name');
 
 const fourthInput = document.createElement('input');
-formInputFieldSecond.append(fourthInput);
+secondInputFields.append(fourthInput);
 fourthInput.setAttribute('type', 'text');
 fourthInput.setAttribute('name', 'last-name');
 fourthInput.setAttribute('id', 'last-name');
 fourthInput.setAttribute('placeholder', 'Last name');
 
-
-const formInputFieldThird = document.createElement('div');
-formInputFieldThird.className = 'input-field';
-formDivInputContainer.append(formInputFieldThird);
+const thirdInputFields = document.createElement('div');
+thirdInputFields.className = 'input-field';
+inputContainer.append(thirdInputFields);
 
 const fifthInput = document.createElement('input');
-formInputFieldThird.append(fifthInput);
+thirdInputFields.append(fifthInput);
 fifthInput.setAttribute('type', 'text');
 fifthInput.setAttribute('name', 'first-name');
 fifthInput.setAttribute('id', 'first-name');
 fifthInput.setAttribute('placeholder', 'First name');
 
 const sixthInput = document.createElement('input');
-formInputFieldThird.append(sixthInput);
+thirdInputFields.append(sixthInput);
 sixthInput.setAttribute('type', 'text');
 sixthInput.setAttribute('name', 'last-name');
 sixthInput.setAttribute('id', 'last-name');
 sixthInput.setAttribute('placeholder', 'Last name');
 
+// Radiobutton 1
+const radioContainer = document.createElement('div');
+radioContainer.className = 'radio-container';
+form.append(radioContainer);
 
-const formDivRadioContainer = document.createElement('div');
-formDivRadioContainer.className = 'radio-container';
-form.append(formDivRadioContainer);
-
-
-const firstRadioButtonContainer = document.createElement('div');
-firstRadioButtonContainer.className = 'radiobutton';
-formDivRadioContainer.append(firstRadioButtonContainer);
+const firstRadionContainer = document.createElement('div');
+firstRadionContainer.className = 'radiobutton';
+radioContainer.append(firstRadionContainer);
 
 const radioButtonNameFirst = document.createElement('div');
 radioButtonNameFirst.className = 'rb-name';
-firstRadioButtonContainer.append(radioButtonNameFirst);
+firstRadionContainer.append(radioButtonNameFirst);
 
-const radioButtonFirstInput = document.createElement('input');
-radioButtonNameFirst.append(radioButtonFirstInput);
-radioButtonFirstInput.setAttribute('type', 'radio');
-radioButtonFirstInput.setAttribute('name', 'join-as');
-radioButtonFirstInput.setAttribute('id', 'join-as-buyer');
+const radioInputFirst = document.createElement('input');
+radioButtonNameFirst.append(radioInputFirst);
+radioInputFirst.setAttribute('type', 'radio');
+radioInputFirst.setAttribute('name', 'join-as');
+radioInputFirst.setAttribute('id', 'join-as-buyer');
 
-const radioButtonFirstLabel = document.createElement('label');
-radioButtonNameFirst.append(radioButtonFirstLabel);
-radioButtonFirstLabel.textContent = 'Join as a buyer';
-radioButtonFirstLabel.setAttribute('for', 'join-as-buyer');
+const radioLabelFirst = document.createElement('label');
+radioButtonNameFirst.append(radioLabelFirst);
+radioLabelFirst.textContent = 'Join as a buyer';
+radioLabelFirst.setAttribute('for', 'join-as-buyer');
 
+const radioTextFirst = document.createElement('div');
+radioTextFirst.className = 'rb-text';
+firstRadionContainer.append(radioTextFirst);
 
-const formDivRadioTextFirst = document.createElement('div');
-formDivRadioTextFirst.className = 'rb-text';
-firstRadioButtonContainer.append(formDivRadioTextFirst);
+const radioTextContentFirst = document.createElement('p');
+radioTextContentFirst.textContent =
+	'I am looking for a Name, Logo or Tagline for my business, brand or product';
+radioTextFirst.append(radioTextContentFirst);
 
-const formDivRadioTextContentFirst = document.createElement('p');
-formDivRadioTextContentFirst.textContent = 'I am looking for a Name, Logo or Tagline for my business, brand or product';
-formDivRadioTextFirst.append(formDivRadioTextContentFirst);
-
-// Radio button 2 
-const secondRadioButtonContainer = document.createElement('div');
-secondRadioButtonContainer.className = 'radiobutton';
-formDivRadioContainer.append(secondRadioButtonContainer);
+// Radio button 2
+const secondRadioConteiner = document.createElement('div');
+secondRadioConteiner.className = 'radiobutton';
+radioContainer.append(secondRadioConteiner);
 
 const radioButtonNameSecond = document.createElement('div');
 radioButtonNameSecond.className = 'rb-name';
-secondRadioButtonContainer.append(radioButtonNameSecond);
+secondRadioConteiner.append(radioButtonNameSecond);
 
-const radioButtonSecondInput = document.createElement('input');
-radioButtonNameSecond.append(radioButtonSecondInput);
-radioButtonSecondInput.setAttribute('type', 'radio');
-radioButtonSecondInput.setAttribute('name', 'join-as');
-radioButtonSecondInput.setAttribute('id', 'join-as-creative');
+const radioinputSecond = document.createElement('input');
+radioButtonNameSecond.append(radioinputSecond);
+radioinputSecond.setAttribute('type', 'radio');
+radioinputSecond.setAttribute('name', 'join-as');
+radioinputSecond.setAttribute('id', 'join-as-creative');
 
-const radioButtonSecondLabel = document.createElement('label');
-radioButtonNameSecond.append(radioButtonSecondLabel);
-radioButtonSecondLabel.textContent = 'Join as a creative or marketplace seller'; 
-radioButtonSecondLabel.setAttribute('for', 'join-as-creative');
-
+const radioInputSecond = document.createElement('label');
+radioButtonNameSecond.append(radioInputSecond);
+radioInputSecond.textContent = 'Join as a creative or marketplace seller';
+radioInputSecond.setAttribute('for', 'join-as-creative');
 
 const formDivRadioTextSecond = document.createElement('div');
 formDivRadioTextSecond.className = 'rb-text';
-secondRadioButtonContainer.append(formDivRadioTextSecond);
+secondRadioConteiner.append(formDivRadioTextSecond);
 
-const formDivRadioTextContentSecond = document.createElement('p');
-formDivRadioTextContentSecond.textContent = 'I plan to submit name ideas, Logo designs or sell names in Domain Marketplace';
-formDivRadioTextSecond.append(formDivRadioTextContentSecond);
+const radioTextContentSecond = document.createElement('p');
+radioTextContentSecond.textContent =
+	'I plan to submit name ideas, Logo designs or sell names in Domain Marketplace';
+formDivRadioTextSecond.append(radioTextContentSecond);
 
-// Checkbox 
+// Checkbox
 const checkboxContainer = document.createElement('div');
 checkboxContainer.className = 'checkbox-continer';
 form.append(checkboxContainer);
@@ -155,16 +152,17 @@ checkboxInput.setAttribute('id', 'offers');
 checkboxContainer.append(checkboxInput);
 
 const checkboxLabel = document.createElement('label');
-checkboxLabel.textContent = 'Allow Squadhelp to send marketing/promotional offers from time to time'; 
+checkboxLabel.textContent =
+	'Allow Squadhelp to send marketing/promotional offers from time to time';
 checkboxLabel.setAttribute('for', 'offers');
 checkboxContainer.append(checkboxLabel);
 
-// Button 
+// Button
 const buttonContainer = document.createElement('div');
 buttonContainer.className = 'button-container';
 form.append(buttonContainer);
 
 const button = document.createElement('button');
-button.textContent = 'Create account'; 
+button.textContent = 'Create account';
 button.setAttribute('type', 'submit');
 buttonContainer.append(button);
